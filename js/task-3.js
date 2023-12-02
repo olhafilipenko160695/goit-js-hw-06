7f85f608a35bcf26fa7c33755336d526145cf27f
olhafilipenko160695 <olyaao95@gmail.com>
2023-12-02 18:44:20 +0100

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,28 @@
 //**Задача 3. Конструктор рядків */
+
+class StringBuilder{
+    constructor(initialValue) {
+        this.value = initialValue;
+    }
+    getValue() {
+        return this.value;
+    }
+    padStart(str) {
+        this.value = str.concat(this.value);
+    }
+    padEnd(str) {
+        this.value = this.value.concat(str);
+    }
+    padBoth(str) {
+    this.value = str.concat(this.value).concat(str)
+    }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue());
