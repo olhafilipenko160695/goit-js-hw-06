37fe90f7f9df58ce7852fb0bb951c3b6974e00ab
olhafilipenko160695 <olyaao95@gmail.com>
2023-12-02 22:55:47 +0100

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,18 @@
 /**Задача 2. Склад */
 
 class Storage{
+    #items;
     constructor(items) {
-        this.items = items; 
+        this.#items = items; 
     }
     getItems() {
-    return this.items; 
+    return this.#items; 
     }
     addItem(newItem) {
-        this.items.push(newItem);   
+    this.#items.push(newItem);   
     }
     removeItem(itemToRemove) {
-    this.items.splice(this.items.indexOf(itemToRemove), 1)
+    this.#items.splice(this.#items.indexOf(itemToRemove), 1)
     }
 
 }
