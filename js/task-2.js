3c56d4dca88af107c7f42616008ecbc9510968c8
olhafilipenko160695 <olyaao95@gmail.com>
2023-12-03 17:29:18 +0100

    1

--- js/task-2.js
+++ js/task-2.js
@@ -12,7 +12,10 @@ class Storage{
     this.#items.push(newItem);   
     }
     removeItem(itemToRemove) {
-    this.#items.splice(this.#items.indexOf(itemToRemove), 1)
+    if (this.#items.indexOf(itemToRemove) !== -1) {
+   this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+
+  }
     }
 
 }
